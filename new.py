74c07b38baf2936b20878c5ce897df74c5ade1fd
Akshay Kalucha <akalucha@hawk.iit.edu>
2025-04-21 23:48:18 -0500

    work done in master

--- new.py
+++ new.py
@@ -1,11 +1,4 @@
 print("mew")
 
 print("new changes in dummy banch")
-
-
-print("new changes in dummy banch")
-def add(a, b):
-    return a + b
-def subtract(a, b):
-    return a - b
-    
+print("some work done in master")
